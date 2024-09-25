174e23c29d56c41874c70d3a52dd0b7270846dd6
jhillacre <joel@403forbidden.ca>
2024-09-25 13:39:19 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <img src="./assets/primevue-logo.png" alt="PrimeVue Logo" />
+  <h2 class="pt-4">PrimeVue Issue Template</h2>
+  <p class="py-4">
+    Please create a test case and attach the link of the codesandbox to your
+    github issue report.
+  </p>
+  <Button label="Button" />
+</template>
